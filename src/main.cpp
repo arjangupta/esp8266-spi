609ee349ec86604715dbadf84701b3972d09219c
Arjan Gupta <arjangupta95@gmail.com>
2022-12-24 19:52:22 -0600

    Add some printlns, fix comparison warning in for loop

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@
 char buff[]="Hello Slave\n";
 
 void setup() {
- Serial.begin(115200); /* begin serial with 9600 baud */
- SPI.begin();  /* begin SPI */
+  Serial.begin(115200); /* begin serial with 9600 baud */
+  SPI.begin();  /* begin SPI */
+  Serial.println("Starting ESP8266 SPI Controller program.");
 }
 
 void loop() {
-  for(int i=0; i<sizeof buff; i++) {  /* transfer buff data per second */
+  Serial.println("Sending data...");
+  for(unsigned int i=0; i<(sizeof buff); i++) {  /* transfer buff data per second */
     SPI.transfer(buff[i]);
   }
   delay(1000);
