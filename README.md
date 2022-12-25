61859512f440ff20bccb725176d9463de40bf8a0
Arjan Gupta <arjangupta95@gmail.com>
2022-12-24 19:24:45 -0600

    Add code from ElectronicWings website

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ESP8266 SPI
+
+Use the [ElectronicWings](https://www.electronicwings.com/nodemcu/nodemcu-spi-with-arduino-ide) guide.
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include<SPI.h>
+
+char buff[]="Hello Slave\n";
 
 void setup() {
-  // put your setup code here, to run once:
+ Serial.begin(115200); /* begin serial with 9600 baud */
+ SPI.begin();  /* begin SPI */
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(int i=0; i<sizeof buff; i++) {  /* transfer buff data per second */
+    SPI.transfer(buff[i]);
+  }
+  delay(1000);
 }
